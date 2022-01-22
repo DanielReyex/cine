6457db9f1ebfa36fd4838eb78081abe6ef02dd8f
DanielReyex <paindaniel36@gmail.com>
2022-01-22 17:06:49 -0500

    first commit

--- app/src/main/java/com/example/cine/HomeFragment.java
+++ app/src/main/java/com/example/cine/HomeFragment.java
@@ -15,6 +15,13 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.home_fragment,container,false);
