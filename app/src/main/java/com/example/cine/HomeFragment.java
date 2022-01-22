921a8915433d94d15f04dfac8327ff22ff6fe88a
DanielReyex <paindaniel36@gmail.com>
2022-01-22 16:00:38 -0500

    first commit

---
+++ app/src/main/java/com/example/cine/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.example.cine;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.home_fragment,container,false);
+
+        setUpToolbar(view);
+
+        return view;
+    }
+
+    private void setUpToolbar(View view){
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity=(AppCompatActivity) getActivity();
+        if (activity != null){
+            activity.setSupportActionBar(toolbar);
+        }
+    }
+
+}
